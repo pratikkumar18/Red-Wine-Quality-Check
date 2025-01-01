401915756109b00e1f6af7eef35a423b4daa02f8
pratikkumar18 <bhagjaidharse@gmail.com>
2025-01-01 00:44:29 +0000

    data transformation added

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from RedWine import logger
 from RedWine.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from RedWine.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
-
+from RedWine.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 
 STAGE_NAME = "DATA INGESTION STAGE"
 try:
@@ -21,6 +21,18 @@ STAGE_NAME = "Data Validation stage"
 try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
    data_ingestion = DataValidationTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+
+STAGE_NAME = "Data Transformation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataTransformationTrainingPipeline()
    data_ingestion.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
--- src/RedWine/config/configuration.py
+++ src/RedWine/config/configuration.py
@@ -1,7 +1,8 @@
 from RedWine.constants import *
 from RedWine.utils.common import read_yaml, create_directories
 from RedWine.entity.config_entity import (DataIngestionConfig,
-                                          DataValidationConfig)
+                                          DataValidationConfig,
+                                          DataTransformationConfig)
 
 class ConfigurationManager:
     def __init__(
@@ -29,7 +30,10 @@ class ConfigurationManager:
             unzip_dir=config.unzip_dir 
         )
 
-        return data_ingestion_config    
+        return data_ingestion_config  
+
+
+
     def get_data_validation_config(self) -> DataValidationConfig:
         config = self.config.data_validation
         schema = self.schema.COLUMNS
@@ -44,3 +48,17 @@ class ConfigurationManager:
         )
 
         return data_validation_config    
+    
+    
+
+    def get_data_transformation_config(self) -> DataTransformationConfig:
+        config = self.config.data_transformation
+
+        create_directories([config.root_dir])
+
+        data_transformation_config = DataTransformationConfig(
+            root_dir=config.root_dir,
+            data_path=config.data_path,
+        )
+
+        return data_transformation_config
