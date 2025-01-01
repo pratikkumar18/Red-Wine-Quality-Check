ec098d6805c30d6f4b63272bc183e99c2a8366a3
pratikkumar18 <bhagjaidharse@gmail.com>
2025-01-01 03:19:56 +0000

    model evaluation added

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from RedWine.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeli
 from RedWine.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from RedWine.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 from RedWine.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
+from RedWine.pipeline.stage_05_model_evaluation import ModelEvaluationTrainingPipeline
 
 STAGE_NAME = "DATA INGESTION STAGE"
 try:
@@ -47,6 +48,17 @@ STAGE_NAME = "Model Trainer stage"
 try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
    data_ingestion = ModelTrainerTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+STAGE_NAME = "Model evaluation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = ModelEvaluationTrainingPipeline()
    data_ingestion.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
